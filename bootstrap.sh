ad40b29f9160946cb29c5fef77bb8c55b1f857a1
Sarlaac <sarlaac@gmail.com>
2022-11-15 16:56:06 -0600

    fixed playbook issues, copy of bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install git python3 python3-pip -y
+python3 -m pip install --upgrade pip
+python3 -m pip install --user ansible
+python3 -m pip install --user ansible-lint
+PATH=/home/$USER/.local/bin:$PATH
+git clone https://github.com/Sarlaac/ansible_mc_server.git
+cd ansible_mc_server
+
+echo "Minecraft server is configured to used 8GB memory by default"
+echo "Search for and edit MEMORY value if the server doesn't have at least 10GB memory."
+echo "Run: ansible-playbook install_mc_docker.yml"
+echo "Become password is sudo user password"
+echo "Minecraft server files installed to /home/docker/docker_mc_server"
+echo ""
+echo "IMPORTANT: when the playbook completes connect to the server on https://server_ip:9443 and set portainer password"
